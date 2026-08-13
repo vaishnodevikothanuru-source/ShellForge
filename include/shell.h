7a36832c7c976dad691d560e5cc513fdcbc637a2
vaishnodevikothanuru-source <vaishnodevikothanuru@gmail.com>
2026-08-13 08:00:25 +0000

    Week 2: Added dynamic input buffer using malloc and realloc

--- include/shell.h
+++ include/shell.h
@@ -2,3 +2,32 @@
 #define SHELL_H
 #define MAX_INPUT 1024
 #endif
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "../include/shell.h"
+#include "../include/input.h"
+
+int main()
+{
+    char *line;
+    printf("=================================\n");
+    printf("%s Version %s\n",SHELL_NAME,VERSION);
+    printf("=================================\n");
+
+    while(1)
+    {
+        printf("myshell> ");
+        line = read_line();
+        if(strcmp(line,"exit")==0)
+        {
+            free(line);
+            break;
+        }
+        if(strlen(line)!=0)
+            printf("You entered : %s\n",line);
+        free(line);
+    }
+    printf("Goodbye!\n");
+    return 0;
+}
---
+++ src/input.c
@@ -0,0 +1,5 @@
+#ifndef SHELL_H
+#define SHELL_H
+#define SHELL_NAME "ShellForge"
+#define VERSION "2.0"
+#endif
