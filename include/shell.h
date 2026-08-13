6e88e8ba18bf906f4a5e5d02f209301dc6ea591e
vaishnodevikothanuru-source <vaishnodevikothanuru@gmail.com>
2026-08-13 08:20:19 +0000

    Week 2: Added dynamic input buffer using malloc and realloc

--- include/shell.h
+++ include/shell.h
@@ -1,33 +1,5 @@
 #ifndef SHELL_H
 #define SHELL_H
-#define MAX_INPUT 1024
+#define SHELL_NAME "ShellForge"
+#define VERSION "2.0"
 #endif
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include "../include/shell.h"
-#include "../include/input.h"
-
-int main()
-{
-    char *line;
-    printf("=================================\n");
-    printf("%s Version %s\n",SHELL_NAME,VERSION);
-    printf("=================================\n");
-
-    while(1)
-    {
-        printf("myshell> ");
-        line = read_line();
-        if(strcmp(line,"exit")==0)
-        {
-            free(line);
-            break;
-        }
-        if(strlen(line)!=0)
-            printf("You entered : %s\n",line);
-        free(line);
-    }
-    printf("Goodbye!\n");
-    return 0;
-}
--- src/main.c
+++ src/main.c
@@ -1,25 +1,29 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
+#include "../include/shell.h"
+#include "../include/input.h"
+
 int main()
 {
-    char input[1024];
-
-    printf("=====================================\n");
-    printf(" Welcome to ShellForge Version 1.0\n");
-    printf("=====================================\n");
+    char *line;
+    printf("=================================\n");
+    printf("%s Version %s\n",SHELL_NAME,VERSION);
+    printf("=================================\n");
 
     while(1)
     {
         printf("myshell> ");
-        if(fgets(input,sizeof(input),stdin)==NULL)
-            break;
-        input[strcspn(input,"\n")]='\0';
-        if(strcmp(input,"exit")==0)
+        line = read_line();
+        if(strcmp(line,"exit")==0)
         {
-            printf("Exiting ShellForge...\n");
+            free(line);
             break;
         }
-        printf("You entered : %s\n",input);
+        if(strlen(line)!=0)
+            printf("You entered : %s\n",line);
+        free(line);
     }
+    printf("Goodbye!\n");
     return 0;
 }
