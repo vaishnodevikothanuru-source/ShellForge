a36567c3015e7d51e3a9cceeb7e150dd7e38bfe6
vaishnodevikothanuru-source <vaishnodevikothanuru@gmail.com>
2026-09-12 08:26:50 +0000

    Week 3:Added command parser using strtok

--- Makefile
+++ Makefile
@@ -1,13 +1,23 @@
+
 CC = gcc
+
 CFLAGS = -Wall -Wextra -g -Iinclude
+
 SRC = src/main.c \
-      src/input.c
+      src/input.c \
+      src/parser.c
+
 TARGET = bin/shellforge
+
 all: $(TARGET)
-$(TARGET):
+
+$(TARGET): $(SRC)
 	mkdir -p bin
 	$(CC) $(CFLAGS) $(SRC) -o $(TARGET)
-run:
+
+run: $(TARGET)
 	./$(TARGET)
+
 clean:
 	rm -rf bin/*
+
--- src/main.c
+++ src/main.c
@@ -1,29 +1,55 @@
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include "../include/shell.h"
-#include "../include/input.h"
 
-int main()
+#include "shell.h"
+#include "input.h"
+#include "parser.h"
+
+int main(void)
 {
     char *line;
-    printf("=================================\n");
-    printf("%s Version %s\n",SHELL_NAME,VERSION);
-    printf("=================================\n");
+    char **tokens;
+    int i;
 
-    while(1)
+    printf("=====================================\n");
+    printf("ShellForge Version 3.0\n");
+    printf("=====================================\n");
+
+    while (1)
     {
         printf("myshell> ");
+
         line = read_line();
-        if(strcmp(line,"exit")==0)
+
+        if (line == NULL)
+        {
+            break;
+        }
+
+        if (strcmp(line, "exit") == 0)
         {
             free(line);
             break;
         }
-        if(strlen(line)!=0)
-            printf("You entered : %s\n",line);
+
+        tokens = parse_line(line);
+
+        printf("\nParsed Tokens\n");
+
+        for (i = 0; tokens[i] != NULL; i++)
+        {
+            printf("argv[%d] = %s\n", i, tokens[i]);
+        }
+
+        free_tokens(tokens);
         free(line);
     }
+
     printf("Goodbye!\n");
+
     return 0;
 }
+
+
